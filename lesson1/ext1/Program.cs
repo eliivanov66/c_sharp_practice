7873a56db4995b06ff28aba9e2eaf77357273de1
Eli <eli.ivanov66@gmail.com>
2022-08-04 22:54:53 +0500

    lesson1 ext1 finished

--- lesson1/ext1/Program.cs
+++ lesson1/ext1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число, квадрат которого вы хотите получить :");
+int value=Convert.ToInt32(Console.ReadLine());        //ввод числа
+value=value*value;
+Console.WriteLine($"Квадрат числа равен : {value}");  //вывод квадрата
