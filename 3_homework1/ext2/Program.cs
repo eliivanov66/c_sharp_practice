010e8fc59226e1253d0907b4b9186f7399197d56
Eli <eli.ivanov66@gmail.com>
2022-08-06 18:35:00 +0500

    Семинар 1 домашнее задание завершено

---
+++ 3_homework1/ext2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*=================================================================================================*/
+/*=== Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. */
+/*=================================================================================================*/
+//метод, мы ведь методы изучали
+double check_input(string message)
+{
+    int input_data_not_ok=1; //данные введены корректно
+    string err_message = "";
+    double temp=0.0;
+    while (input_data_not_ok==1)
+    {
+        try
+        {
+            Console.Clear();
+            Console.Write($"{err_message}{message}");
+            temp=Convert.ToDouble(Console.ReadLine());
+            input_data_not_ok=0;
+        }
+        catch (SystemException) //если это не integer
+        {
+            input_data_not_ok=1;
+            err_message="Некорректные данные. ";
+        }
+    }
+    return temp;
+}
+//переменные
+double a=0.0;   //первое число
+double b=0.0;   //второе число
+double c=0.0;   //третье число
+double max=0.0; //максимальное число
+//получение данных
+a=check_input("Введите первое число: ");
+b=check_input("Введите второе число: ");
+c=check_input("Введите третье число: ");
+//логика
+max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+//вывод результата
+Console.WriteLine($"Максимальное число из чисел {a} {b} {c} равно {max}");
---
+++ 3_homework1/ext4/Program.cs
@@ -0,0 +1,52 @@
+﻿/*======================================================================================================================*/
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N */
+/*======================================================================================================================*/
+//метод, мы ведь методы изучали
+double check_input(string message)
+{
+    int input_data_not_ok=1; //данные введены корректно
+    string err_message = "";
+    double temp=0.0;
+    while (input_data_not_ok==1)
+    {
+        try
+        {
+            Console.Clear();
+            Console.Write($"{err_message}{message}");
+            temp=Convert.ToDouble(Console.ReadLine());
+            input_data_not_ok=0;
+        }
+        catch (SystemException) //если это не integer
+        {
+            input_data_not_ok=1;
+            err_message="Некорректные данные. ";
+        }
+    }
+    return temp;
+}
+
+//переменные
+double input=0.0;  //вводные данные
+int temp_input=0;  //промежуточная конверсия  
+double result=0.0; //переменная для вывода результата
+
+input=check_input("Введите число ");
+//избавляемся от хвоста после запятой
+temp_input=Convert.ToInt32(input);
+input=Convert.ToDouble(temp_input);
+
+result=0.0;
+Console.WriteLine($"Чётные числа между 0 и {input}:");
+while (Math.Abs(result)<Math.Abs(input))
+{
+    if (result!=0.0) Console.Write($" {result}");
+    //выбираем направление отчёта
+    if (input<0.0)
+    {
+        result=result-2.0;  
+    }
+    if (input>0.0)
+    {
+        result=result+2.0;
+    }
+} 
