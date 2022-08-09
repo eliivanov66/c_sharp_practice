64036736fb6b9b6a824a0ffa3e10abe48f768193
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2022-08-09 11:53:45 +0500

    Домашнее задание после семинара 2 выполнено

---
+++ 3_homework2/ext2/Program.cs
@@ -0,0 +1,87 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+//метод для коррекции ввода
+int check_int_input(string message)
+{
+    bool input_data_not_ok=true; //введены некорректные данные
+    int input=0;                 //число для ввода
+    string err_message="";
+    while (input_data_not_ok) //пока введены некорректные данные
+    {
+        Console.Clear();
+        Console.WriteLine($"{err_message}{message} :");
+        try
+        {
+            input=Convert.ToInt32(Console.ReadLine());
+            input_data_not_ok=false; //считаем что данные введены корректно
+        }
+        catch (SystemException) 
+        {
+            input_data_not_ok=true; //пока введены некорректные данные
+            err_message="Неправильный ввод. ";
+        }
+    }
+    return input;
+}
+//метод вычленяющий нужный нам знак
+int show_sign_by_number(int input, int sign)
+{
+    if (sign>=1)
+    {
+        return (input % Convert.ToInt32(Math.Pow(10.0, Convert.ToDouble(sign)))/
+                       Convert.ToInt32(Math.Pow(10.0, Convert.ToDouble(sign-1)) ));
+    }
+    else
+    {
+        Console.WriteLine("show_sign_by_number: Параметры метода заданы некорректно");
+        return -1;
+    }
+}
+//метод возвращающий количество знаков в числе
+//метод возвращающий количество знаков в числе
+int show_sign_count (int input)
+{
+    int temp=input;
+    int sign_count=0;
+    //проверка сколько знаков в числе
+    while (temp>0)
+    {
+        temp=temp/10;
+        sign_count++;
+    }
+    return sign_count;
+}
+
+ConsoleKeyInfo choise; //ввод клавиши
+Console.Clear();
+Console.WriteLine("Для значений ввода значений нажмите любую клавишу, для выхода из программы нажмите q");
+choise=Console.ReadKey();
+while (choise.Key!=ConsoleKey.Q)
+{
+    int number=check_int_input("Введите число "); //ввод числа, в котором ищем цифру
+    int sign=check_int_input($"Введите номер цифры числа {number} числа для отображения"); //ввод номера цифры, которую ишем в числе
+    int sign_count=show_sign_count(number); //находим сколько цифр в числе
+    int result=show_sign_by_number(number, sign); //находим цифру в числе
+
+    Console.Clear();
+    if (sign_count>=sign) //если в есть цифра под нужным нам номером
+    {
+        Console.WriteLine($"Вы ввели число {number} и хотели найти его {sign} цифру, результат: в числе {sign_count} цифр, {sign}ая цифра это {result}");
+
+    }
+    else
+    {
+         Console.WriteLine($"Вы ввели число {number} и хотели найти его {sign} цифру, результат: в числе {sign_count} цифр, {sign}ая цифра отсутсвует");
+    }
+
+
+    Console.WriteLine("Для значений ввода значений нажмите любую клавишу, для выхода из программы нажмите q");
+    choise=Console.ReadKey();
+}
---
+++ 3_homework2/ext3/Program.cs
@@ -0,0 +1,87 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+//метод для коррекции ввода
+int check_int_input(string message)
+{
+    bool input_data_not_ok=true; //введены некорректные данные
+    int input=0;                 //число для ввода
+    string err_message="";
+    while (input_data_not_ok) //пока введены некорректные данные
+    {
+        Console.Clear();
+        Console.WriteLine($"{err_message}{message} :");
+        try
+        {
+            input=Convert.ToInt32(Console.ReadLine());
+            input_data_not_ok=false; //считаем что данные введены корректно
+        }
+        catch (SystemException) 
+        {
+            input_data_not_ok=true; //пока введены некорректные данные
+            err_message="Неправильный ввод. ";
+        }
+    }
+    return input;
+}
+//метод вычленяющий нужный нам знак
+int show_sign_by_number(int input, int sign)
+{
+    if (sign>=1)
+    {
+        return (input % Convert.ToInt32(Math.Pow(10.0, Convert.ToDouble(sign)))/
+                       Convert.ToInt32(Math.Pow(10.0, Convert.ToDouble(sign-1)) ));
+    }
+    else
+    {
+        Console.WriteLine("show_sign_by_number: Параметры метода заданы некорректно");
+        return -1;
+    }
+}
+//метод возвращающий количество знаков в числе
+//метод возвращающий количество знаков в числе
+int show_sign_count (int input)
+{
+    int temp=input;
+    int sign_count=0;
+    //проверка сколько знаков в числе
+    while (temp>0)
+    {
+        temp=temp/10;
+        sign_count++;
+    }
+    return sign_count;
+}
+
+ConsoleKeyInfo choise; //ввод клавиши
+Console.Clear();
+Console.WriteLine("Для значений ввода значений нажмите любую клавишу, для выхода из программы нажмите q");
+choise=Console.ReadKey();
+string[] day_of_week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+while (choise.Key!=ConsoleKey.Q)
+{
+    int number=0;
+    while (number<1 || number>7)
+    {
+        number=check_int_input("Введите номер дня недели"); //ввод числа номера недели
+    }
+
+    Console.Clear();
+    if (number>=6) //если в есть цифра под нужным нам номером
+    {
+        Console.WriteLine($"Вы ввели номер дня недели {number}, это {day_of_week[number-1].ToLower()} и это выходной");
+
+    }
+    else
+    {
+         Console.WriteLine($"Вы ввели номер дня недели {number}, это {day_of_week[number-1].ToLower()} и это будний день");
+    }
+
+
+    Console.WriteLine("Для значений ввода значений нажмите любую клавишу, для выхода из программы нажмите q");
+    choise=Console.ReadKey();
+}
