d8f22d061b5e9b86e1a98e46ccc3e5e1d8916796
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2022-08-15 23:32:27 +0500

    Семинар 4. Задача 2 решена

--- 2_practice4/general_task_2/Librarys.cs
+++ 2_practice4/general_task_2/Librarys.cs
@@ -111,4 +111,20 @@ public class Librarys
             return -1;
         }
     }
+    //метод конверсии десятичного числа в двоичное
+    public static int convert_bin_to_dec(int arg_input, int arg_base)
+    {
+        int result=0;
+        int sign_count=show_sign_count_int(arg_input); //сколько разрядов в числе
+        Console.WriteLine($"Количество разрядов числа sign_count = {sign_count}");
+        //111101
+        for (int i = 0; i < sign_count; i++)
+        {
+            int sign_value=show_sign_by_number(arg_input, i + 1); //показывает значение в текущем разряде
+            Console.WriteLine($"Значение в разряде sign_value = {sign_value}");
+            result=result + sign_value * Convert.ToInt32(Math.Pow(2.0, i));      //значение результата для текущего разряда
+            Console.WriteLine($"Промежуточный результат result = {result}");
+        }
+        return result;
+    }
 }
--- 2_practice4/general_task_2/Program.cs
+++ 2_practice4/general_task_2/Program.cs
@@ -1,24 +1,6 @@
 ﻿//библиотека методов из прошлого задания
 using static Librarys;
 
-//метод конверсии десятичного числа в двоичное
-int convert_bin_to_dec(int arg_input, int arg_base)
-{
-    int result=0;
-    int sign_count=show_sign_count_int(arg_input); //сколько разрядов в числе
-    Console.WriteLine($"Количество разрядов числа sign_count = {sign_count}");
-    //111101
-    for (int i = 0; i < sign_count; i++)
-    {
-        int sign_value=show_sign_by_number(arg_input, i + 1); //показывает значение в текущем разряде
-        Console.WriteLine($"Значение в разряде sign_value = {sign_value}");
-        result=result + sign_value * Convert.ToInt32(Math.Pow(2.0, i));      //значение результата для текущего разряда
-        Console.WriteLine($"Промежуточный результат result = {result}");
-    }
-    return result;
-}
-
-
 //код задачи
 int input=0; //ввод от пользователя
 
