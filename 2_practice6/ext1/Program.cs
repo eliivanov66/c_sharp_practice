944b6e1704b9580884a2a54bb9a3b1337f6594f8
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2022-08-23 12:17:56 +0500

    Practice 6 modification code for ext1

--- 2_practice6/ext1/Program.cs
+++ 2_practice6/ext1/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Составить частотный словарь элементов двумерного массива */
-
+using System.Globalization;
 
 //заполнение массива
 void fill_array_char(ref char[,] input)
@@ -7,9 +7,8 @@ void fill_array_char(ref char[,] input)
     for (int i=0; i<input.GetLength(0); i++)
     {
         for (int j=0; j<input.GetLength(1); j++)
-        {
-            //input[i,j]=Convert.ToChar(System.Random.Shared.Next(65, 91)); 
-            input[i,j]=Convert.ToChar(System.Random.Shared.Next(97, 123));    
+        {   
+            input[i,j]=Convert.ToChar(System.Random.Shared.Next(0, 65535));    
         }
     }
 }
@@ -27,34 +26,18 @@ void print_array_char(char[,] input)
     }
 }
 
-//печать массива
-void print_array_double(double[] input)
-{
-    Console.Write("[");
-    for (int i=0; i<input.GetLength(0); i++)
-    {
 
-        Console.Write($"{input[i]};");   
-        
-    }
-    Console.Write("]");
-    Console.WriteLine();
-}
-
-double[] power_array (char[,] arg_input_array, string arg_alphafet)
+double[] power_array (string arg_input, string arg_alphafet)
 {
     double[] buffer_array=new double[arg_alphafet.Length];
     double perzent_sum=0.0;
     for (int i=0; i<arg_alphafet.Length; i++)
     {
-        for (int j=0; j<arg_input_array.GetLength(0); j++)
+        for (int j=0; j<arg_input.Length; j++)
         {
-            for (int k=0; k<arg_input_array.GetLength(1); k++)
-            {
-            if (arg_alphafet[i]==arg_input_array[j,k])  buffer_array[i]=buffer_array[i]+1.0;
-            }     
+            if (arg_alphafet[i]==arg_input[j])  buffer_array[i]=buffer_array[i]+1.0; 
         } 
-        buffer_array[i]=100.0*buffer_array[i]/(arg_input_array.GetLength(0)*arg_input_array.GetLength(1));
+        buffer_array[i]=100.0*buffer_array[i]/(arg_input.Length);
         perzent_sum=perzent_sum+buffer_array[i];
         if (buffer_array[i]>0.0) Console.WriteLine($"{arg_alphafet[i]}: {buffer_array[i]} %");
     }
@@ -62,18 +45,21 @@ double[] power_array (char[,] arg_input_array, string arg_alphafet)
     return buffer_array;
 }
 
-string abc ="abcdefghijklmnopqrstuwvyxz"; //алфавит 65-90 ASCII
-char[,] input_array=new char[10,10];    //массив - текст
-double [] result_array = new double[abc.Length]; //массив частот
+//string abc ="abcdefghijklmnopqrstuwvyxz"; //алфавит 65-90 ASCII
+string abc =""; //таблица ASCII 255 элементов
+//заполнение словаря
+for (int i=0; i<65535; i++)
+{
+    abc=String.Concat(abc, Convert.ToChar(i));
+}
 
 Console.Clear();
-fill_array_char(ref input_array);
-Console.WriteLine("Сгенерирован следующий массив: ");
-print_array_char(input_array);
+//чтение исходного текста
+Console.WriteLine("Введите текст для анализа частот заполнения: ");
+string input=$"{Console.ReadLine()}";            //исходный текст
+double [] result_array = new double[abc.Length]; //массив частот
+
+Console.WriteLine("Получен следующий исходный текст: ");
+Console.WriteLine(input);
 Console.WriteLine("Массив мощностей: ");
-result_array=power_array(input_array, abc);
-
-
-
-//Console.WriteLine($"Получен символ кодом {temp} : {System.Convert.ToChar(temp)}");
-//Console.WriteLine(Convert.ToChar(65));
+result_array=power_array(input, abc);
