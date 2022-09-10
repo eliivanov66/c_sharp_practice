7411544d416fded569b6b515ee28edb345cbf720
eliivanov66 <109104693+eliivanov66@users.noreply.github.com>
2022-09-10 16:07:24 +0500

    snake can moving, eat points, but not growing yet

--- snake/My_Snake.cs
+++ snake/My_Snake.cs
@@ -12,48 +12,6 @@ public class My_Snake
             Console.WriteLine();
         }
     }
-    public static void rotate_figure(ref char[,] arg_figure, int arg_angle, bool arg_direction)
-    {
-        int size_x=arg_figure.GetLength(1); //4 -- 3
-        int size_y=arg_figure.GetLength(0); //3 -- 4
-
-        char[,] result_figure=new char[size_x,size_y]; //3x4
-        init_field(ref result_figure, '0');
-
-        int rotate_count=arg_angle/90;
-        if (arg_direction)
-        {
-            for (int k=0; k<rotate_count; k++)
-            { 
-                //поворот на 90 по часовой стрелке
-                for (int i = 0; i < arg_figure.GetLength(0); i++) //4x3
-                {
-                    for (int j = 0; j < arg_figure.GetLength(1); j++) //4x3
-                    {
-                        result_figure[j,size_y-i - 1]=
-                        arg_figure[i,j];
-                    }
-                }
-            }
-        }
-        else
-        {
-            for (int k=0; k<rotate_count; k++)
-            {
-                //поворот на 90 против часовой стрелке
-                for (int i = 0; i < arg_figure.GetLength(0); i++)
-                {
-                    for (int j = 0; j < arg_figure.GetLength(1); j++)
-                    {
-                        result_figure[size_x-j - 1 ,i]=
-                        result_figure[i,j];
-                    }
-                }
-            }
-        }
-        arg_figure=result_figure;
-
-    }
     public static char[,] place_figures(char[,] arg_field, char[,] arg_figure, int arg_x, int arg_y)
     {
         //Console.Clear();
@@ -67,17 +25,17 @@ public class My_Snake
             }
         }
         //фигура меньше поля, всегда
-        if ( (arg_field.GetLength(0)>=arg_figure.GetLength(0)+arg_x) &&
-             (arg_field.GetLength(1)>=arg_figure.GetLength(1)+arg_y))
+        if ( (arg_field.GetLength(0)>=arg_figure.GetLength(0)+arg_y) &&
+             (arg_field.GetLength(1)>=arg_figure.GetLength(1)+arg_x))
         {
             for (int i=0; i<arg_figure.GetLength(0);i++)
             {
                 for (int j = 0; j < arg_figure.GetLength(1); j++)
                 {
-                    if (buffer_field[i+ arg_x,j+ arg_y]!='0') 
+                    if (buffer_field[i+ arg_y,j+ arg_x]!=' ') 
                         continue;
                     else
-                        buffer_field[ i+ arg_x, j+ arg_y]=arg_figure[i,j];
+                        buffer_field[ i+ arg_y, j+ arg_x]=arg_figure[i,j];
                 }
             }
         }
@@ -98,14 +56,14 @@ public class My_Snake
     {
         bool result=false;
         //фигура меньше поля, всегда
-        if ( (arg_field.GetLength(0)>=arg_figure.GetLength(0)+arg_x) &&
-             (arg_field.GetLength(1)>=arg_figure.GetLength(1)+arg_y))
+        if ( (arg_field.GetLength(0)>=arg_figure.GetLength(0)+arg_y) &&
+             (arg_field.GetLength(1)>=arg_figure.GetLength(1)+arg_x))
         {
             for (int i = 0; i < arg_figure.GetLength(0); i++)
             {
                 for (int j = 0; j < arg_figure.GetLength(1); j++)
                 {
-                    if ((arg_field[i + arg_x, j + arg_y]!='0') && (arg_field[i + arg_x , j + arg_y]==arg_figure[i,j]))  
+                    if ((arg_field[i + arg_y, j + arg_x]!=' ') && (arg_field[i + arg_y , j + arg_x]==arg_figure[i,j]))  
                     {    
                         result=true; break;
                     }
@@ -114,47 +72,5 @@ public class My_Snake
             }
         }
         return result;
-    }
-    public static (bool,int) line_analyse(char[,] arg_field)
-    {
-        bool line=false;
-        int line_number=0;
-        for (int i = 0; i < arg_field.GetLength(0); i++)
-        {
-            if (line) break;
-            for (int j=0; j<arg_field.GetLength(1); j++)
-            {
-                if (arg_field[i,j]!='0') //просмотр значений на последней строке
-                {
-                    line=true;
-                    line_number=i;
-                }
-                else
-                {
-                    line=false;
-                    line_number=0;
-                    break;
-                }
-            }
-        }
-        return (line,line_number);
-    }
-    public static char[,] remove_line(char[,] arg_field, int arg_line_number)
-    {
-        int m=0;
-        char[,] buffer_field=new char[arg_field.GetLength(0), arg_field.GetLength(1)];
-        for (int j=0; j < arg_field.GetLength(1); j++)
-        {
-            buffer_field[0,j]='0';
-        }
-        for (int i = 1; i < arg_field.GetLength(0); i++) //i это строка
-        {
-            if (i<=arg_line_number) m=-1; else m=0;
-            for (int j = 0; j < arg_field.GetLength(1); j++)
-            {
-                buffer_field[i,j]=arg_field[i + m, j];
-            }
-        }
-        return buffer_field;
     }
 }
--- snake/Program.cs
+++ snake/Program.cs
@@ -1,60 +1,76 @@
 
 using static My_Snake;
-char _0='0';
 char _1=Convert.ToChar(9632);
-char[,] field      =new char[16,16];
-char[,] fill_field =new char[16,16];
-char[,] figure;
-char[,] line       =new char[1,4] {{_1,_1,_1,_1}};
-char[,] square     =new char[2,2] {{_1,_1},{_1,_1}};
-char[,] angle_left =new char[3,2] {{_0,_1},{_0,_1},{_1,_1}};
-char[,] angle_right=new char[3,2] {{_1,_0},{_1,_0},{_1,_1}};
-char[,] sig_left   =new char[3,2] {{_1,_0},{_1,_1},{_0,_1}};
-char[,] sig_right  =new char[3,2] {{_0,_1},{_1,_1},{_1,_0}};
-char[,] triangle   =new char[2,3] {{_0,_1,_0},{_1,_1,_1}};
-char[,] star       =new char[3,3] {{_0,_1,_0},{_1,_1,_1},{_0,_1,_0}};
-char[,] snake_left =new char[3,3] {{_1,_0,_0},{_1,_1,_1},{_0,_0,_1}};
-char[,] snake_right=new char[3,3] {{_0,_0,_1},{_1,_1,_1},{_1,_0,_0}};
-
-//List<char[,]> list_figures = new List<char[,]> {line,square,angle_left, angle_right,sig_left,sig_right,triangle,star, snake_left,snake_right};
-List<char[,]> list_figures = new List<char[,]> {line,square,angle_left, angle_right,sig_left,sig_right,triangle};
-
+char[,] field      =new char[16,32];
+char[,] fill_field =new char[16,32];
+char[,] snake = new char [1,1] {{Convert.ToChar(9632)}};
+char[,] target = new char [1,1] {{'+'}};
 //переменные
-int coord_x=0;                        //координата фигуры по X
-int coord_y=field.GetLength(1)/2;     //координата фигуры по Y
-int size_x=0;                         //размер фигуры по X
-int size_y=0;                         //размер фигуры по Y
-bool collision=false;                 //фигура попала на заполненное поле
-bool pause_game=false;                //пауза игры
-bool game_over=false;                 //игра окончена
-int score=0;                          //очки
-int speed=500;                        //скорость задержки между движениями, мсек
-ConsoleKeyInfo choise;                //переменная ввода клавиши
+int snake_coord_x=0;                    //координата головы змейки по X
+int snake_coord_y=field.GetLength(0)/2; //координата головы змейки по Y
+int moving_direction=2;                 //направление движения
+int snake_lenght=0;                     //длина змеи
+int target_coord_x=Random.Shared.Next(0, field.GetLength(1));//координата цели по X
+int target_coord_y=Random.Shared.Next(0, field.GetLength(0));//координата цели по Y
+bool collision=false;                   //фигура попала на заполненное поле
+bool pause_game=false;                  //пауза игры
+bool game_over=false;                   //игра окончена
+int score=0;                            //очки
+int speed=500;                          //скорость задержки между движениями, мсек
+ConsoleKeyInfo choise;                  //переменная ввода клавиши
 //инициализация поля и поля результатов, первой фигуры
-init_field(ref fill_field,'0');
-init_field(ref field,'0');
+init_field(ref fill_field,' ');
+init_field(ref field,' ');
 print_figure(fill_field);
-figure=list_figures[Random.Shared.Next(0,list_figures.Count)]; //берём любую фигуру из листа
 //поток отрисовки фигуры на плоскости
 new Thread(() =>
 {
   while (true)
   {
     //снятие геометрии текущей фигуры
-    size_x=figure.GetLength(0);
-    size_y=figure.GetLength(1);
     //проверка достигла ли фигура пола или есть ли колизии с другими объектами, если да, то новая фигура
     //рисование поля и фигуры на нём
-    field=place_figures(fill_field, figure, coord_x, coord_y); //рисование фигуры на поле
+    switch (moving_direction)
+    {
+        case 0: 
+            if (!pause_game) snake_coord_y--;
+            if ((snake_coord_y) < 0) snake_coord_y=field.GetLength(0)-1; //движение вверх
+            break;
+        case 1: 
+            if (!pause_game) snake_coord_y++;
+            if ((snake_coord_y) >= field.GetLength(0)) snake_coord_y=0; //движение вниз
+            break;
+        case 2:
+            if (!pause_game) snake_coord_x++;
+            if ((snake_coord_x) >= field.GetLength(1)) snake_coord_x=0; //движение вправо
+            break;
+        case 3: 
+            if (!pause_game) snake_coord_x--;
+            if ((snake_coord_x) < 0) snake_coord_x=field.GetLength(1)-1; //движение влево
+            break;
+    }
+    field=place_figures(fill_field, snake, snake_coord_x, snake_coord_y); //рисование фигуры на поле
+    field=place_figures(field, target, target_coord_x, target_coord_y); //рисование фигуры на поле
     print_figure(field);
+
     //провера переполнения игрового поля
-    collision=colision_field(fill_field, figure, coord_x , coord_y); //проверка на пересечение с фигурами уже стоящими на поле
-    if (collision) game_over=true; //поле переполнено
-    Console.WriteLine($"Управление: ВВЕРХ - вращение, ВЛЕВО/ВПРАВО - смещение");
-    Console.WriteLine($"Управление: ПРОБЕЛ - пауза, A/S - изменеие скорости падения, Q/ESC - выход");
-    Console.WriteLine($"У вас {score} очков, скорость падения {speed}");
+    collision=(target_coord_x==snake_coord_x) && (target_coord_y==snake_coord_y);
+    if (collision) //касаение змейкой цели
+    {
+        snake_lenght++;
+        target_coord_x=Random.Shared.Next(0, field.GetLength(1));
+        target_coord_y=Random.Shared.Next(0, field.GetLength(0));
+        char[,] snake = new char [1,1] {{Convert.ToChar(9632)}};
+    }
+    if (!pause_game) 
+        Console.WriteLine($"Управление: ВВЕРХ/ВНИЗ/ВЛЕВО/ВПРАВО - движение, ПРОБЕЛ - пауза");
+    else
+        Console.WriteLine($"ПАУЗА АКТИВНА, ПРОБЕЛ - отменить паузу");
+    score=snake_lenght;
+    Console.WriteLine($"У вас {score} очков, скорость движения {speed} (A/S - изменение скорости)");
+    Console.WriteLine($"x {snake_coord_x}, y {snake_coord_y}");
     Thread.Sleep(speed);
-    if (!pause_game) coord_x++;
+    //if (!pause_game) coord_x++;
 
     //выход из приложения
     if (game_over)
@@ -85,28 +101,29 @@ new Thread(() =>
         {
             pause_game=!pause_game;
         }
-        //вращение фигуры   
+        //движение вверх   
         if ((choise.Key==ConsoleKey.UpArrow) && (!pause_game))
         {
-            if ((coord_y+size_x) > field.GetLength(1)) coord_y--;
-            rotate_figure(ref figure, 90, true); //вращение по часовой стрелке
+            //coord_y--;
+            moving_direction=0;
+        }
+        //движение вниз   
+        if ((choise.Key==ConsoleKey.DownArrow) && (!pause_game))
+        {
+            //coord_y++;
+            moving_direction=1;
         }
         //движение вправо
         if ((choise.Key==ConsoleKey.RightArrow) && (!pause_game))
         {   
-            collision=colision_field(fill_field, figure, coord_x , coord_y); //проверка на пересечение с фигурами уже стоящими на поле
-            if (coord_y+size_y<field.GetLength(1) && !collision) coord_y++; //движение вправо
+            //coord_x++;
+            moving_direction=2;
         }
         //движение влево
         if ((choise.Key==ConsoleKey.LeftArrow) && (!pause_game))
         {
-            collision=colision_field(fill_field, figure, coord_x , coord_y); //проверка на пересечение с фигурами уже стоящими на поле
-            if (coord_y>0 && !collision) coord_y--; 
-        }
-        //движение вниз
-        if ((choise.Key==ConsoleKey.DownArrow) && (!pause_game))
-        {
-            if (coord_x+size_x<field.GetLength(0)) coord_x++; //движение вниз
+            //coord_x--;
+            moving_direction=3;
         }
         if ((choise.Key==ConsoleKey.S) && (!pause_game))
         {
